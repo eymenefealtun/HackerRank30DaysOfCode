ce70f7fd15af3895fdccfb2b647bdfd14e43b29c
eymenefealtun <eymenefealtun@hotmail.com>
2023-07-24 21:53:17 +0300

    Day-06 added

---
+++ Day-06-Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        Console.Clear();
+
+        for (int i = 1; i <= 10; i++)
+            Console.WriteLine($"{n} x {i} = {n * i}");
+
+        Console.ReadLine();
+    }
+}
