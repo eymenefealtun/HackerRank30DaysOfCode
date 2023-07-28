f94b31c5a51847a2647ba5c3f4dcfb7ef739ad57
eymenefealtun <eymenefealtun@hotmail.com>
2023-07-28 16:09:33 +0300

    Day-09 added

---
+++ Day-09-Dictionaries&Maps/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        IDictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            phoneBook.Add(input[0], input[1]);
+        }
+
+        string queryString = "";
+
+        while ((queryString = Console.ReadLine()) != null)
+        {
+            if (phoneBook.ContainsKey(queryString))
+                Console.WriteLine(queryString + "=" + phoneBook[queryString]);
+            else
+                Console.WriteLine("Not found");
+        }
+
+        Console.ReadLine();
+
+    }
+}
