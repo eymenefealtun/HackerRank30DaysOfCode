6a4f2504f71f6225f7fa01e7588a55dc82e5024a
eymenefealtun <eymenefealtun@hotmail.com>
2023-07-29 22:35:02 +0300

    Day-11 added

---
+++ Day-11-BinaryNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string[] strings = Convert.ToString(n, 2).Split('0');
+
+        int numberOfOnes = strings.Max().Length;
+
+        Console.Clear();
+
+        Console.WriteLine(numberOfOnes);
+
+        Console.ReadLine();
+    }
+}
