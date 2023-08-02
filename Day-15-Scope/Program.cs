b176a90fb9ef3f8389bc3c2806d32847ed71f060
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-02 21:29:38 +0300

    Day-15 added

---
+++ Day-15-Scope/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(int[] array)
+    {
+        elements = array;
+    }
+
+    internal void computeDifference()
+    {
+        int min = elements.Min();
+        int max = elements.Max();
+
+        maximumDifference = max - min;
+    }
+
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+        Console.ReadLine();
+    }
+}
