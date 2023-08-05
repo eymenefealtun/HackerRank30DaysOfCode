385ed1f11b9c48b458a0107bf15d045cd35ca33b
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-05 17:19:13 +0300

    Day-17 added

---
+++ Day-17-Exceptions&StringToInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+		try
+		{
+			Console.WriteLine(Convert.ToInt32(S));
+		}
+		catch (Exception)
+		{
+			Console.WriteLine("Bad String");
+		}
+		Console.ReadLine();
+
+    }
+}
