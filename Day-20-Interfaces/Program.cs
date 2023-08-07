ea1a54b536adc02b6010b6dd5a75682a27f78a4e
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-07 20:37:20 +0300

    Day-20 addded

---
+++ Day-20-Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public interface AdvancedArithmetic
+{
+    int divisorSum(int n);
+}
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        List<int> numbers = new List<int>();
+
+        for (int i = 1; i <= n; i++)
+            if (n % i == 0)
+                numbers.Add(i);
+
+        return numbers.Sum();
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine());
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+    }
+}
