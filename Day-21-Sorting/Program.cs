2f2fd717538e03acd01e6a3f9b0e9a8c25d6b418
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-08 21:08:34 +0300

    Day 21 added

---
+++ Day-21-Sorting/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> givenList = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        // Write your code here
+        int swap = 0;
+        int length = givenList.Count;
+
+        for (int i = 0; i < length; i++)
+        {
+            // Track number of elements swapped during a single array traversal
+
+            for (int j = 0; j < length - 1; j++)
+            {
+                // Swap adjacent elements if they are in decreasing order
+                if (givenList[j] > givenList[j + 1])
+                {
+                    int current = givenList[j];
+                    givenList[j] = givenList[j + 1];
+                    givenList[j + 1] = current;
+                    swap++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (swap == 0)
+                break;
+        }
+
+        Console.WriteLine("Array is sorted in {0} swaps.", swap);
+        Console.WriteLine("First Element: {0}", givenList[0]);
+        Console.WriteLine("Last Element: {0}", givenList[length - 1]);
+
+        Console.ReadLine();
+    }
+}
---
+++ Day-21-Sorting/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Day-21-Sorting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day-21-Sorting")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1145877b-f1a4-474a-8e54-f2a75ed0712a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
