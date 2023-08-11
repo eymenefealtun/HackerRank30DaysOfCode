2d77d82fd5e8b4e32516571992eae1eff2c23db5
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-11 20:56:30 +0300

    Day-23 added

---
+++ Day-23-BinarySearchTrees/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+class Node
+{
+    public Node left, right;
+    public int data;
+    public Node(int data)
+    {
+        this.data = data;
+        left = right = null;
+    }
+}
+class Solution
+{
+
+    static int getHeight(Node root)
+    {
+        //Write your code here
+        decimal left = root.left != null ? getHeight(root.left) : -1;
+        decimal right = root.right != null ? getHeight(root.right) : -1;
+
+        return (int)Math.Max(left, right) + 1;
+    }
+
+    static Node insert(Node root, int data)
+    {
+        if (root == null)
+        {
+            return new Node(data);
+        }
+        else
+        {
+            Node cur;
+            if (data <= root.data)
+            {
+                cur = insert(root.left, data);
+                root.left = cur;
+            }
+            else
+            {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+    static void Main(String[] args)
+    {
+        Node root = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            root = insert(root, data);
+        }
+        int height = getHeight(root);
+        Console.WriteLine(height);
+        Console.ReadLine();
+
+    }
+}
